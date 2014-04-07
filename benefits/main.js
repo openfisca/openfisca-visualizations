4d8aa67203367786ae0440b1a869f5d77a50a20b
Christophe Benz <cbenz@easter-eggs.com>
2014-04-07 12:10:42 +0200

    Add load indication.

--- benefits/main.js
+++ benefits/main.js
@@ -9,79 +9,86 @@ require([
 ], function(domReady, $, _, d3, nv, queryString) {
   'use strict';
 
-  $.ajax({
-    type: 'GET',
-    url: queryString.simulate_url, // jshint ignore:line
-  })
-  .then(function(data) {
-    var salbrut;
-    var valueIndex = 0;
-    var createNodes = function(node, nodes, baseValue) {
-      if (_.isUndefined(nodes)) {
-        nodes = [];
-      }
-      if (_.isUndefined(baseValue)) {
-        baseValue = 0;
-      }
-      if (node.code === 'salbrut') {
-        salbrut = node.values;
-      }
-      var children = node.children;
-      if (children) {
-        var childBaseValue = baseValue;
-        _.each(node.children, function (child) {
-          nodes.concat(createNodes(child, nodes, childBaseValue));
-          childBaseValue += child.values[valueIndex];
-        });
-      }
+  nv.debug = false;
 
-      var value = node.values[valueIndex];
-      if ( ! children && _.filter(node.values, function(n) { return n !== 0; } ).length > 0) {
-        var column = {
-          baseValue: baseValue,
-          code: value.code,
-        };
-        _.extend(column, node);
-        nodes.push(column);
-      }
-      return nodes;
-    };
-    var nodes = createNodes(data.value);
+  var container = d3.select('.container');
 
-    return _.filter(_.map(nodes, function(node) {
-      if (node.values[0] < 0) {
-        return {
-          label: node.name,
-          value: node.values[0]
-        };
-      }
-    }), function(item) {
-      return (!_.isUndefined(item));
+  if (queryString.simulate_url) { // jshint ignore:line
+    $.ajax({
+      type: 'GET',
+      url: queryString.simulate_url, // jshint ignore:line
+    })
+    .then(function(data) {
+      var salbrut;
+      var valueIndex = 0;
+      var createNodes = function(node, nodes, baseValue) {
+        if (_.isUndefined(nodes)) {
+          nodes = [];
+        }
+        if (_.isUndefined(baseValue)) {
+          baseValue = 0;
+        }
+        if (node.code === 'salbrut') {
+          salbrut = node.values;
+        }
+        var children = node.children;
+        if (children) {
+          var childBaseValue = baseValue;
+          _.each(node.children, function (child) {
+            nodes.concat(createNodes(child, nodes, childBaseValue));
+            childBaseValue += child.values[valueIndex];
+          });
+        }
+
+        var value = node.values[valueIndex];
+        if ( ! children && _.filter(node.values, function(n) { return n !== 0; } ).length > 0) {
+          var column = {
+            baseValue: baseValue,
+            code: value.code,
+          };
+          _.extend(column, node);
+          nodes.push(column);
+        }
+        return nodes;
+      };
+      var nodes = createNodes(data.value);
+
+      return _.filter(_.map(nodes, function(node) {
+        if (node.values[0] < 0) {
+          return {
+            label: node.name,
+            value: node.values[0]
+          };
+        }
+      }), function(item) {
+        return (!_.isUndefined(item));
+      });
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.error('Fetch API data error : ', jqXHR, textStatus, errorThrown);
+    })
+    .then(function(data) {
+      nv.addGraph(function() {
+        var chart = nv.models.pieChart()
+        .x(function(d) { return d.label; })
+        .y(function(d) { return d.value; })
+        .showLabels(true)
+        .labelThreshold(0.05)
+        .labelType('percent')
+        .donut(true)
+        .donutRatio(0.35);
+
+        container.append('svg')
+          .attr('id', 'chart')
+          .datum(data)
+          .transition().duration(350)
+          .call(chart);
+
+        return chart;
+      });
     });
-  })
-  .fail(function(jqXHR, textStatus, errorThrown) {
-    console.error('Fetch API data error : ', jqXHR, textStatus, errorThrown);
-  })
-  .then(function(data) {
-    //Donut chart example
-    nv.addGraph(function() {
-      var chart = nv.models.pieChart()
-      .x(function(d) { return d.label; })
-      .y(function(d) { return d.value; })
-      .showLabels(true) //Display pie labels
-      .labelThreshold(0.05) //Configure the minimum slice size for labels to show up
-      .labelType('percent') //Configure what type of data to show in the label. Can be "key", "value" or "percent"
-      .donut(true) //Turn on Donut mode. Makes pie chart look tasty!
-      .donutRatio(0.35) //Configure how big you want the donut hole size to be.
-      ;
-
-      d3.select('#chart')
-        .datum(data)
-        .transition().duration(350)
-        .call(chart);
-
-      return chart;
-    });
-  });
+  } else {
+    container.text('"simulate_url" GET parameter is missing.');
+  }
 
 });
--- rd/main.js
+++ rd/main.js
@@ -9,99 +9,107 @@ require([
 ], function(domReady, $, _, d3, nv, queryString) {
   'use strict';
 
+  nv.debug = false;
+
+  var container = d3.select('.container');
+
   var axes = [{
     count: 50,
     max: 80000,
     min: 0,
     name: 'sali'
   }];
-  $.ajax({
-    type: 'POST',
-    url: queryString.simulateUrl,
-    data: {
-      axes: JSON.stringify(axes),
-      decomposition: JSON.stringify([
-        {code: 'nivvie'},
-        {code: 'sali'},
-        {code: 'revdisp'},
-      ]),
-    }
-  })
-  .then(function (data) {
-    var parsedData = [];
 
-    var nivvieObj = data.value[0];
-    var revdispObj = data.value[2];
-    var saliObj = data.value[1];
+  if (queryString.simulate_url) { // jshint ignore:line
+    $.ajax({
+      type: 'POST',
+      url: queryString.simulate_url, // jshint ignore:line
+      data: {
+        axes: JSON.stringify(axes),
+        decomposition: JSON.stringify([
+          {code: 'nivvie'},
+          {code: 'sali'},
+          {code: 'revdisp'},
+        ]),
+      }
+    })
+    .then(function (data) {
+      var parsedData = [];
 
-    var revdispMinValue = d3.min(revdispObj.values);
-    // console.log();
+      var nivvieObj = data.value[0];
+      var revdispObj = data.value[2];
+      var saliObj = data.value[1];
 
-    /* Add sal datas */
-    //
-    // _.each(nivvieObj.values, function (val) {
+      var revdispMinValue = d3.min(revdispObj.values);
+      // console.log();
 
-    // });
-    parsedData.push({
-      key: 'Revenu disponible',
-      values: [],
-      bar: true,
-      color: '#ccf',
-    });
-    _.each(revdispObj.values, function (val, i) {
-      parsedData[0].values.push([saliObj.values[i]]);
-      parsedData[0].values[i].push(val);
-    });
+      /* Add sal datas */
+      //
+      // _.each(nivvieObj.values, function (val) {
 
-    parsedData.push({
-      key: 'Niveau de vie',
-      values: [],
-      color: '#ccf',
-    });
-    _.each(nivvieObj.values, function (val, i) {
-      parsedData[1].values.push([saliObj.values[i]]);
-      parsedData[1].values[i].push(val);
-    });
-
-
-    nv.addGraph(function() {
-      var chart = nv.models.linePlusBarChart()
-        .margin({top: 30, right: 80, bottom: 50, left: 70})
-        //We can set x data accessor to use index. Reason? So the bars all appear evenly spaced.
-        .x(function(d, i) { return i; })
-        .y(function(d) {return d[1]; });
-
-      chart.xAxis.tickFormat(function(d) {
-        d = d / 49 * 80000;
-        return d3.format('f')(d)+ '€';
+      // });
+      parsedData.push({
+        key: 'Revenu disponible',
+        values: [],
+        bar: true,
+        color: '#ccf',
+      });
+      _.each(revdispObj.values, function (val, i) {
+        parsedData[0].values.push([saliObj.values[i]]);
+        parsedData[0].values[i].push(val);
       });
 
-      chart.y1Axis.tickFormat(d3.format(',f'));
+      parsedData.push({
+        key: 'Niveau de vie',
+        values: [],
+        color: '#ccf',
+      });
+      _.each(nivvieObj.values, function (val, i) {
+        parsedData[1].values.push([saliObj.values[i]]);
+        parsedData[1].values[i].push(val);
+      });
 
-      chart.y2Axis.tickFormat(function(d) { return d3.format(',f')(d) + '€'; });
 
-      chart.bars.forceY([0]);
+      nv.addGraph(function() {
+        var chart = nv.models.linePlusBarChart()
+          .margin({top: 30, right: 80, bottom: 50, left: 70})
+          //We can set x data accessor to use index. Reason? So the bars all appear evenly spaced.
+          .x(function(d, i) { return i; })
+          .y(function(d) {return d[1]; });
 
-      d3.select('#chart')
-        .datum(parsedData)
-        .transition()
-        .duration(0)
-        .call(chart);
+        chart.xAxis.tickFormat(function(d) {
+          d = d / 49 * 80000;
+          return d3.format('f')(d)+ '€';
+        });
 
-      var actualYScale = chart.y2Axis.scale(),
-          actualYScaleDomain = actualYScale.domain();
+        chart.y1Axis.tickFormat(d3.format(',f'));
 
-      actualYScale.domain([revdispMinValue, actualYScaleDomain[1]]);
-      chart.y2Axis.scale(actualYScale);
+        chart.y2Axis.tickFormat(function(d) { return d3.format(',f')(d) + '€'; });
 
-      nv.utils.windowResize(chart.update);
+        chart.bars.forceY([0]);
 
-      $('.nv-bars').attr('display', 'none');
+        container.append('svg')
+          .attr('id', 'chart')
+          .datum(parsedData)
+          .transition()
+          .duration(0)
+          .call(chart);
 
-      return chart;
+        var actualYScale = chart.y2Axis.scale(),
+            actualYScaleDomain = actualYScale.domain();
+
+        actualYScale.domain([revdispMinValue, actualYScaleDomain[1]]);
+        chart.y2Axis.scale(actualYScale);
+
+        nv.utils.windowResize(chart.update);
+
+        $('.nv-bars').attr('display', 'none');
+
+        return chart;
+      });
     });
-  });
+  } else {
+    container.text('"simulate_url" GET parameter is missing.');
+  }
+
 });
-
-
-
